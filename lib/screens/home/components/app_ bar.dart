5c3e035df69629631ca60915bd9eb8cab607ee21
zxq <461890276@qq.com>
2022-03-19 10:32:40 +0800

    提交更新

--- lib/screens/home/components/app_ bar.dart
+++ lib/screens/home/components/app_ bar.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import "package:flutter/material.dart";
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:order_food/constants.dart';
