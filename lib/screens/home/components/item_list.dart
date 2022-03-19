0bac81d3bff255f683aaf8bdde1b825c1614a2db
zxq <461890276@qq.com>
2022-03-19 12:02:53 +0800

    提交更新

---
+++ lib/screens/home/components/item_list.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:order_food/screens/home/components/item_card.dart';
+
+class ItemList extends StatelessWidget {
+  const ItemList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          ItemCard(
+            svgSrc: "assets/icons/burger_beer.svg",
+            title: "Burger & Beer",
+            shopName: "MacDonald's",
+            press: () {},
+          ),
+          ItemCard(
+            svgSrc: "assets/icons/chinese_noodles.svg",
+            title: "Chinese & Noodles",
+            shopName: "Wendys",
+            press: () {},
+          ),
+          ItemCard(
+            svgSrc: "assets/icons/burger_beer.svg",
+            title: "Burger & Beer",
+            shopName: "MacDonald's",
+            press: () {},
+          ),
+          ItemCard(
+            svgSrc: "assets/icons/burger_beer.svg",
+            title: "Burger & Beer",
+            shopName: "MacDonald's",
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
